800b98b25a41c901730a414e021321c73e79c821
MoonlightSV <stalkervolosh@gmail.com>
2019-05-18 09:58:18 +0900

    Added ability to type commands

--- Assistant.py
+++ Assistant.py
@@ -2,31 +2,39 @@ from Friday.Friday import Friday as Assistant
 import speech_recognition as sr
 
 
-def run_transcription_loop():
+def run_transcription_loop(type):
     # Слушает команды
-
-    r = sr.Recognizer()
-    with sr.Microphone() as source:
+    if type == 'текст':
         while True:
-            Assistant.talkToMe('Слушаю...')
-            audio = r.listen(source)
-            print('Думаю...')
+            command = input('Жду команду: ')
+            Assistant.handle_action(command)
 
-            try:
-                command = r.recognize_google(audio, language="ru-RU")
-                print('Вы сказали: ' + command)
-                Assistant.handle_action(command)
-            except sr.UnknownValueError:
-                Assistant.talkToMe("Не поняла что вы сказали, попробуйте еще раз...")
-            except sr.RequestError as e:
-                print("Не могу получить результат из сервисов Google Speech Recognition: {}".format(e))
-            except Exception as e:
-                print("Не могу обработать текст: {}".format(e))
+    elif type == 'голос':
+        r = sr.Recognizer()
+        with sr.Microphone() as source:
+            while True:
+                Assistant.talkToMe('Слушаю...')
+                r.pause_threshold = 0.5
+                r.adjust_for_ambient_noise(source, duration=0.5)
+                audio = r.listen(source)
+                print('Думаю...')
+
+                try:
+                    command = r.recognize_google(audio, language="ru-RU")
+                    print('Вы сказали: ' + command)
+                    Assistant.handle_action(command)
+                except sr.UnknownValueError:
+                    Assistant.talkToMe("Не поняла что вы сказали, попробуйте еще раз...")
+                except sr.RequestError as e:
+                    print("Не могу получить результат из сервисов Google Speech Recognition: {}".format(e))
+                except Exception as e:
+                    print("Не могу обработать текст: {}".format(e))
 
 
 def main():
     Assistant.talkToMe('Приветствую вас')
-    run_transcription_loop()
+    type = input('Как вы хотите вводить команды голос/текст: ')
+    run_transcription_loop(type)
 
 
 if __name__ == '__main__':
